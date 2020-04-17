5d1802973a4fd0713de71d750e2e862917bd06bc
vinayarora4 <vinayarora9896@gmail.com>
2020-04-17 12:32:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect, state } from 'react';
-import { View, StyleSheet, FlatList, ImageBackground } from 'react-native';
+import { View, StyleSheet, FlatList, Text, ImageBackground, ActivityIndicator } from 'react-native';
 import Header from './components/Header';
 import axios from 'axios';
 import ListItem from './components/ListItem';
-
-
-
+import Colors from './constent/Colors';
 
 const App: () => React$Node = () => {
   const [isData, isDataSet] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   useEffect(() => {
@@ -16,35 +15,36 @@ const App: () => React$Node = () => {
       then((response) => {
 
         isDataSet(response.data.list);
+        setIsLoading(false);
 
       });
 
   }, []);
 
+  if (isLoading) {
+
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size='large' color={Colors.primaryColor} />
+      </View>);
+  };
 
   return (
-
     <View style={styles.imageContainer}>
       <ImageBackground style={styles.backgroundImage} source={require('./assets/backgroundrain.jpg')} >
         <Header />
-
         <View style={styles.lisContainer}>
-
-
           <ListItem
             temp='Temperature'
             humidity='Humidity'
             wind='Wind Speed' />
-
 
           <FlatList data={isData} keyExtractor={item => item.dt} renderItem={({ item }) =>
             <ListItem
               temp={item.main.temp}
               humidity={item.main.humidity}
               wind={item.wind.speed}
-
             />} />
-
 
           {/* {
             isData.map((itemData) => {
@@ -63,17 +63,7 @@ const App: () => React$Node = () => {
         </View>
       </ImageBackground>
 
-
-
-
-
     </View>
-
-
-
-
-
-
 
   );
 };
@@ -91,6 +81,11 @@ const styles = StyleSheet.create({
   },
   lisContainer: {
     padding: 15
+  },
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 
 });
